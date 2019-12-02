959dc66f80b8bb3253ac492fc5ef5949535478da
Eric Moreno <emoreno@caltech.edu>
2019-12-02 15:42:18 -0800

    added SV-branch option

--- gnn.py
+++ gnn.py
@@ -46,6 +46,9 @@ class GraphNet(nn.Module):
             self.fo3_v = nn.Linear(int(self.hidden/2), self.Do).cuda()
 
         if self.vv_branch:
+            #self.fc_1 = nn.Linear(2*self.Do, self.Do).cuda()
+            #self.fc_2 = nn.Linear(self.Do, int(self.Do/2)).cuda()
+            #self.fc_3 = nn.Linear(int(self.Do/2), self.n_targets).cuda()
             self.fc_fixed = nn.Linear(2*self.Do, self.n_targets).cuda()
         else:
             self.fc_fixed = nn.Linear(self.Do, self.n_targets).cuda()
@@ -158,6 +161,9 @@ class GraphNet(nn.Module):
         
         ### Classification MLP ###
         if self.vv_branch:
+            #N = nn.functional.relu(self.fc_1(torch.cat([N, N_v],1)))
+            #N = nn.functional.relu(self.fc_2(N))
+            #N = self.fc_3(N)
             N =self.fc_fixed(torch.cat([N, N_v],1))
         else:
             N = self.fc_fixed(N)
@@ -273,11 +279,7 @@ class GraphNetnoSV(nn.Module):
         self.assign_matrices()
         self.vv_branch = vv_branch
         self.sv_branch = sv_branch
-        if self.sv_branch: 
-            self.assign_matrices_SV()
-        if self.vv_branch:
-            self.assign_matrices_SVSV()
-        
+           
         self.Ra = torch.ones(self.Dr, self.Nr)
         self.fr1 = nn.Linear(2 * self.P + self.Dr, self.hidden).cuda()
         self.fr2 = nn.Linear(self.hidden, int(self.hidden/2)).cuda()
@@ -285,29 +287,12 @@ class GraphNetnoSV(nn.Module):
         self.fr1_pv = nn.Linear(self.S + self.P + self.Dr, self.hidden).cuda()
         self.fr2_pv = nn.Linear(self.hidden, int(self.hidden/2)).cuda()
         self.fr3_pv = nn.Linear(int(self.hidden/2), self.De).cuda()
-        if self.vv_branch:
-            self.fr1_vv = nn.Linear(2 * self.S + self.Dr, self.hidden).cuda()
-            self.fr2_vv = nn.Linear(self.hidden, int(self.hidden/2)).cuda()
-            self.fr3_vv = nn.Linear(int(self.hidden/2), self.De).cuda()
         
-        if self.sv_branch:
-            self.fo1 = nn.Linear(self.P + self.Dx + (2 * self.De), self.hidden).cuda()
-            self.fo2 = nn.Linear(self.hidden, int(self.hidden/2)).cuda()
-            self.fo3 = nn.Linear(int(self.hidden/2), self.Do).cuda()
-        else: 
-            self.fo1 = nn.Linear(self.P + self.Dx + (self.De), self.hidden).cuda()
-            self.fo2 = nn.Linear(self.hidden, int(self.hidden/2)).cuda()
-            self.fo3 = nn.Linear(int(self.hidden/2), self.Do).cuda()
+        self.fo1 = nn.Linear(self.P + self.Dx + (self.De), self.hidden).cuda()
+        self.fo2 = nn.Linear(self.hidden, int(self.hidden/2)).cuda()
+        self.fo3 = nn.Linear(int(self.hidden/2), self.Do).cuda()
         
-        if self.vv_branch:
-            self.fo1_v = nn.Linear(self.S + self.Dx + (2 * self.De), self.hidden).cuda()
-            self.fo2_v = nn.Linear(self.hidden, int(self.hidden/2)).cuda()
-            self.fo3_v = nn.Linear(int(self.hidden/2), self.Do).cuda()
-
-        if self.vv_branch:
-            self.fc_fixed = nn.Linear(2*self.Do, self.n_targets).cuda()
-        else:
-            self.fc_fixed = nn.Linear(self.Do, self.n_targets).cuda()
+        self.fc_fixed = nn.Linear(self.Do, self.n_targets).cuda()
         #self.gru = nn.GRU(input_size = self.Do, hidden_size = 20, bidirectional = False).cuda()
             
     def assign_matrices(self):
@@ -319,28 +304,8 @@ class GraphNetnoSV(nn.Module):
             self.Rs[s, i] = 1
         self.Rr = (self.Rr).cuda()
         self.Rs = (self.Rs).cuda()
-    
-    def assign_matrices_SV(self):
-        self.Rk = torch.zeros(self.N, self.Nt)
-        self.Rv = torch.zeros(self.Nv, self.Nt)
-        receiver_sender_list = [i for i in itertools.product(range(self.N), range(self.Nv))]
-        for i, (k, v) in enumerate(receiver_sender_list):
-            self.Rk[k, i] = 1
-            self.Rv[v, i] = 1
-        self.Rk = (self.Rk).cuda()
-        self.Rv = (self.Rv).cuda()
 
-    def assign_matrices_SVSV(self):
-        self.Rl = torch.zeros(self.Nv, self.Ns)
-        self.Ru = torch.zeros(self.Nv, self.Ns)
-        receiver_sender_list = [i for i in itertools.product(range(self.Nv), range(self.Nv)) if i[0]!=i[1]]
-        for i, (l, u) in enumerate(receiver_sender_list):
-            self.Rl[l, i] = 1
-            self.Ru[u, i] = 1
-        self.Rl = (self.Rl).cuda()
-        self.Ru = (self.Ru).cuda()
-
-    def forward(self, x, y):
+    def forward(self, x):
         ###PF Candidate - PF Candidate###
         Orr = self.tmul(x, self.Rr)
         Ors = self.tmul(x, self.Rs)
@@ -355,83 +320,27 @@ class GraphNetnoSV(nn.Module):
         Ebar_pp = self.tmul(E, torch.transpose(self.Rr, 0, 1).contiguous())
         del E
         
-        if self.sv_branch:
-            ####Secondary Vertex - PF Candidate### 
-            Ork = self.tmul(x, self.Rk)
-            Orv = self.tmul(y, self.Rv)
-            B = torch.cat([Ork, Orv], 1)
-            ### First MLP ###
-            B = torch.transpose(B, 1, 2).contiguous()
-            B = nn.functional.relu(self.fr1_pv(B.view(-1, self.S + self.P + self.Dr)))
-            B = nn.functional.relu(self.fr2_pv(B))
-            E = nn.functional.relu(self.fr3_pv(B).view(-1, self.Nt, self.De))
-            del B
-            E = torch.transpose(E, 1, 2).contiguous()
-            Ebar_pv = self.tmul(E, torch.transpose(self.Rk, 0, 1).contiguous())
-            Ebar_vp = self.tmul(E, torch.transpose(self.Rv, 0, 1).contiguous())
-            del E
-
-        ###Secondary vertex - secondary vertex###
-        if self.vv_branch:
-            Orl = self.tmul(y, self.Rl)
-            Oru = self.tmul(y, self.Ru)
-            B = torch.cat([Orl, Oru], 1)
-            ### First MLP ###
-            B = torch.transpose(B, 1, 2).contiguous()
-            B = nn.functional.relu(self.fr1_vv(B.view(-1, 2 * self.S + self.Dr)))
-            B = nn.functional.relu(self.fr2_vv(B))
-            E = nn.functional.relu(self.fr3_vv(B).view(-1, self.Ns, self.De))
-            del B
-            E = torch.transpose(E, 1, 2).contiguous()
-            Ebar_vv = self.tmul(E, torch.transpose(self.Rl, 0, 1).contiguous())
-            del E
 
         ####Final output matrix for particles###
         
-        if self.sv_branch: 
-            C = torch.cat([x, Ebar_pp, Ebar_pv], 1)
-            del Ebar_pp
-            del Ebar_pv
-            C = torch.transpose(C, 1, 2).contiguous()
-            ### Second MLP ###
-            C = nn.functional.relu(self.fo1(C.view(-1, self.P + self.Dx + (2 * self.De))))
-            C = nn.functional.relu(self.fo2(C))
-            O = nn.functional.relu(self.fo3(C).view(-1, self.N, self.Do))
-            del C
-        else: 
-            C = torch.cat([x, Ebar_pp], 1)
-            del Ebar_pp
-            C = torch.transpose(C, 1, 2).contiguous()
-            ### Second MLP ###
-            C = nn.functional.relu(self.fo1(C.view(-1, self.P + self.Dx + (self.De))))
-            C = nn.functional.relu(self.fo2(C))
-            O = nn.functional.relu(self.fo3(C).view(-1, self.N, self.Do))
-            del C
-            
-        if self.vv_branch:
-            ####Final output matrix for particles### 
-            C = torch.cat([y, Ebar_vv, Ebar_vp], 1)
-            del Ebar_vv
-            del Ebar_vp
-            C = torch.transpose(C, 1, 2).contiguous()
-            ### Second MLP ###
-            C = nn.functional.relu(self.fo1_v(C.view(-1, self.S + self.Dx + (2 * self.De))))
-            C = nn.functional.relu(self.fo2_v(C))
-            O_v = nn.functional.relu(self.fo3_v(C).view(-1, self.Nv, self.Do))
-            del C
+
+        C = torch.cat([x, Ebar_pp], 1)
+        del Ebar_pp
+        C = torch.transpose(C, 1, 2).contiguous()
+        ### Second MLP ###
+        C = nn.functional.relu(self.fo1(C.view(-1, self.P + self.Dx + (self.De))))
+        C = nn.functional.relu(self.fo2(C))
+        O = nn.functional.relu(self.fo3(C).view(-1, self.N, self.Do))
+        del C
+
         
         #Taking the sum of over each particle/vertex
         N = torch.sum(O, dim=1)
         del O
-        if self.vv_branch:
-            N_v = torch.sum(O_v,dim=1)
-            del O_v
         
         ### Classification MLP ###
-        if self.vv_branch:
-            N =self.fc_fixed(torch.cat([N, N_v],1))
-        else:
-            N = self.fc_fixed(N)
+
+        N = self.fc_fixed(N)
 
         return N 
             
